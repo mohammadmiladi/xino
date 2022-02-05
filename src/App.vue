54147cc5bdcf6d578f30b1fb16d5632ecd7d1de9
m.miladi <mohammadmiladi.ie@gmail.com>
2022-02-05 20:09:53 +0330

    footer added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Home />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Home from "./components/Home.vue";
+import Footer from "./components/common/Footer.vue";
 
 export default {
   name: "App",
   components: {
     Home,
+    Footer,
   },
 };
 </script>
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @click="goToMohammadSite()" class="footer-style">
+    Copyright 2021 Mohammad Miladi
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+  methods: {
+    goToMohammadSite() {
+      window.open("https://mohammadmiladi.ir", "_blank");
+    },
+  },
+};
+</script>
+
+<style>
+.footer-style {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 7px;
+  position: fixed;
+  bottom: 0;
+  color: blue;
+  font-size: 20px;
+  width: 100%;
+  height: 20px;
+  background: rgb(214, 214, 255);
+}
+.footer-style:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
