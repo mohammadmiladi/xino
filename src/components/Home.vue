87804b6d534f67cd9bb056d5a5d7210473f934e3
m.miladi <mohammadmiladi.ie@gmail.com>
2022-02-05 14:37:58 +0330

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-if="pageNumber == 1">
+      <FirstPage />
+    </div>
+    <div v-if="pageNumber == 2">
+      <SecondPage />
+    </div>
+    <div v-if="pageNumber == 3">
+      <ThirdPage />
+    </div>
+  </div>
+</template>
+
+<script>
+import FirstPage from "../components/FirstPage.vue";
+import SecondPage from "../components/SecondPage.vue";
+import ThirdPage from "../components/ThirdPage.vue";
+
+export default {
+  name: "home",
+  components: {
+    FirstPage,
+    SecondPage,
+    ThirdPage,
+  },
+  data() {
+    return {
+      pageNumber: 1,
+    };
+  },
+};
+</script>
+
+<style></style>
